778b9b1864fc1d4e2d5d3d2deb04e4386c922fc9
Tomasz Gajewski <tomasz.gajewski0922@gmail.com>
2023-03-12 21:29:10 +0100

    6.2 Calulator class testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+        int addResult = calculator.add(2,2);
+        int subtractResult = calculator.subtract(2,1);
+
+        if (addResult == 4) {
+            System.out.println("Metoda add() działa poprawnie");
+        } else {
+            System.out.println("Błąd metody add()");
+        }
+
+        if (subtractResult == 1) {
+            System.out.println("Metoda subtract() działa poprawnie");
+        } else {
+            System.out.println("Błąd metody subtract()");
+        }
     }
 }
