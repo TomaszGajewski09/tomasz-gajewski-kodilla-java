d8f7f0df76f8de0899188d7b760d9387fea401d9
Tomasz Gajewski <tomasz.gajewski0922@gmail.com>
2023-03-13 12:32:48 +0100

    6.3 Added OddNumbersExterminator class and testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,20 +5,6 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        int addResult = calculator.add(2,2);
-        int subtractResult = calculator.subtract(2,1);
 
-        if (addResult == 4) {
-            System.out.println("Metoda add() działa poprawnie");
-        } else {
-            System.out.println("Błąd metody add()");
-        }
-
-        if (subtractResult == 1) {
-            System.out.println("Metoda subtract() działa poprawnie");
-        } else {
-            System.out.println("Błąd metody subtract()");
-        }
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,50 @@
+package com.kodilla.testing.collection;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionTestSuite {
+    @BeforeEach
+    void before() {
+        System.out.println("OddNumbersExterminator class test - start");
+    }
+
+    @AfterEach
+    void after() {
+        System.out.println("OddNumbersExterminator class test - end");
+    }
+    @DisplayName("When creat OddNumbersExterminator " +
+            "and use method exterminate() with empty list as an argument" +
+            "should return empty list")
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        List<Integer> list = new ArrayList<>();
+        List<Integer> expectedResult = new ArrayList<>();
+        //When
+        List<Integer> result = oddNumbersExterminator.exterminate(list);
+        //Then
+        Assertions.assertEquals(expectedResult, result);
+
+    }
+
+    @DisplayName("When create OddNumbersExterminator " +
+            "and use method exterminate() with numbers list as an argument " +
+            "should return even numbers list")
+    @Test
+    void testOddNumbersExterminatorNormalList() {
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        List<Integer> expectedResult = new ArrayList<>(Arrays.asList(2,4,6,8,10));
+        //When
+        List<Integer> result = oddNumbersExterminator.exterminate(list);
+        //Then
+        Assertions.assertEquals(expectedResult,result);
+
+    }
+}
