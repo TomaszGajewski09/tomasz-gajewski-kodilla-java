9b1c2277d435c24390e58edbaa81e049613803f3
TomaszGajewski09 <tomasz.gajewski0922@gmail.com>
2023-11-10 16:49:20 +0100

    31.1 Zadanie

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -6,16 +6,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class GoogleTestingApp {
-    public static final String SEARCHFIELD = "q";
+    public static WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+
 
     public static void main(String[] args) {
-        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
-        driver.get("https://www.google.com");
 
+//        googleToKodillaQuery();
+        ebayQuery();
+    }
+
+    private static void googleToKodillaQuery() {
+        final String SEARCHFIELD = "q";
+        driver.get("https://www.google.com");
         driver.findElement(By.xpath("//*[@id=\"L2AGLb\"]/div")).click();
 
         WebElement searchField = driver.findElement(By.name(SEARCHFIELD));
         searchField.sendKeys("Kodilla");
+        searchField.submit();
+    }
+
+    private static void ebayQuery() {
+        final String SEARCHFIELD = "_nkw";
+        driver.get("https://www.ebay.com/");
+
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));
+        searchField.sendKeys("Laptop");
         searchField.submit();
     }
 }
